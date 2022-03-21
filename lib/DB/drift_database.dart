e345f0e5b421c6b4925ee15652ce5db77243600f
shaohuakdi@gmail.com <shaohuakdi@gmail.com>
2022-03-20 21:52:44 -0400

    Partial Database Intergration
    need help on home_page and 1 query

--- lib/DB/drift_database.dart
+++ lib/DB/drift_database.dart
@@ -21,21 +21,32 @@
 ///               isFinished (background)
 /// //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-import 'package:moor/moor.dart';
-import 'package:moor_flutter/moor_flutter.dart';
+import 'dart:io';
 
-part 'moor_database.g.dart';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+//import 'package:moor/moor.dart';
+//import 'package:moor_flutter/moor_flutter.dart';
+
+part 'drift_database.g.dart';
+//part 'moor_database.g.dart';
 
 class Accounts extends Table {
   //by default, the column properties with the autoIncrement sub-function is set as primary key
-  IntColumn get userID => integer().autoIncrement()();
+  IntColumn get cardID => integer()();
+  // IntColumn get userID => integer()();
   //can set length constrain like below; separate parameters with comma
-  TextColumn get username => text().withLength(min: 10)();
+  TextColumn get username => text().withLength(min: 1)();
   TextColumn get email => text()();
   TextColumn get pw => text().withLength(min: 8)();
+
+  @override
+  Set<Column> get primaryKey => {cardID};
 }
 
-//if each user can only have 1 account, this table and related DAO can be deleted
+/*//if each user can only have 1 account, this table and related DAO can be deleted
 class Acs extends Table {
   IntColumn get cardID => integer()();
   //add below to userID as custom constraint in case if card without account is allowed
@@ -45,35 +56,186 @@ class Acs extends Table {
   // override for primary key can be done like (when auto increment isn't implemented for w/e reason)
   @override
   Set<Column> get primaryKey => {cardID};
-}
+}*/
 
 class Transactions extends Table {
   IntColumn get transactionID => integer().autoIncrement()();
   IntColumn get cardID => integer()();
+  //IntColumn get cardID => integer().nullable().references(Accounts, #cardID)();
   DateTimeColumn get time => dateTime()();
   RealColumn get amount => real()();
   // for type, true is debit and false is credit
-  BoolColumn get type => boolean().withDefault(const Constant(true))();
+  //BoolColumn get type => boolean().withDefault(const Constant(true))();
   BoolColumn get isValidated => boolean().withDefault(const Constant(false))();
   BoolColumn get isFinished => boolean().withDefault(const Constant(false))();
 }
+class CardTransaction{
+  final DateTime time;
+  final double amount;
 
-@UseMoor(
-    tables: [Accounts, Acs, Transactions], daos: [AccountDao, TransactionDao])
+  CardTransaction(this.time, this.amount);
+}
+
+LazyDatabase _openConnection() {
+  // the LazyDatabase util lets us find the right location for the file async.
+  return LazyDatabase(() async {
+    // put the database file, called db.sqlite here, into the documents folder
+    // for your app.
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
+//@UseMoor
+@DriftDatabase(
+    tables: [Accounts,  Transactions],
+)
+
 // _$AppDatabase is the name of the generated class
 class AppDatabase extends _$AppDatabase {
-  AppDatabase()
+
+  AppDatabase() : super(_openConnection());
+
+/*  AppDatabase()
       : super((FlutterQueryExecutor.inDatabaseFolder(
-          path: 'db.sqlite',
-          logStatements: true,
-        )));
+    path: 'db.sqlite',
+    logStatements: true,
+  )));*/
 
   /// db migration can be done here; not implemented yet/still need to learn if needed
   @override
   int get schemaVersion => 1;
+
+  get time => null;
+
+  get amount => null;
+
+
+  //
+  // login related
+  //
+  //for forgot password, we can do username check, and if its valid, pull user info (presumably email) using loggedInUserInfo
+  Future<Expression<bool>> usernameExists(String uname) async {
+    return existsQuery(
+        select(accounts)..where((row) => row.username.equals(uname)));
+  }
+
+  Future<Expression<bool>> cardExists(int cardID) async{
+    return existsQuery(
+        select(accounts)..where((row) => row.cardID.equals(cardID)));
+  }
+
+  Future<Expression<bool>> passwordCorrect(String uname, String password) async{
+    //can cascade multiple where()s like below
+    return existsQuery(select(accounts)
+      ..where((row) => row.username.equals(uname))
+      ..where((row) => row.pw.equals(password)));
+  }
+
+  //need to pull userID from the row/table generated from this in UI and pass onto home_page
+  Future<Account> loggedInUserInfo(String uname) {
+    return (select(accounts)..where((row) => row.username.equals(uname)))
+        .getSingle();
+  }
+
+  String loggedInUserCardID(String uname){
+    return customSelect(
+        //query works fine
+        //'SELECT * FROM books;',
+        'SELECT cardID FROM Accounts WHERE username = ?',
+        variables: [Variable.withString(uname)],
+    ).toString();
+  }
+  // presumably superuser/admin stuff
+  Future<List<Account>> getAllAccounts() => select(accounts).get();
+
+  Stream<List<Account>> watchAllAccounts() => select(accounts).watch();
+  /*Stream<Account> watchAccountByID(int id) {
+    return (select(accounts)..where((t) => t.userID.equals(id))).watchSingle();
+  }*/
+
+  Future insertAccount(Insertable<Account> account) =>
+      into(accounts).insert(account);
+  Future updateAccount(Insertable<Account> account) =>
+      update(accounts).replace(account);
+  Future deleteAccount(Insertable<Account> account) =>
+      delete(accounts).delete(account);
+
+
+  /*Future<Ac> loggedInUserCard(int uID) {
+    return (select(acs)..where((row) => row.userID.equals(uID))).getSingle();
+  }
+
+  // presumably superuser/admin stuff
+  Future<List<Ac>> getAllACs() => select(acs).get();
+
+  Stream<List<Ac>> watchAllACs() => select(acs).watch();
+  Stream<List<Ac>> watchUserACs(int id) {
+    return (select(acs)..where((row) => row.userID.equals(id))).watch();
+  }
+
+  Future insertAcs(Insertable<Ac> ac) => into(acs).insert(ac);
+  Future updateAcs(Insertable<Ac> ac) => update(acs).replace(ac);
+  Future deleteAcs(Insertable<Ac> ac) => delete(acs).delete(ac);*/
+
+  Stream<List<CardTransaction>> watchCardTransactions(int cardID) {
+    return customSelect(
+      //query works fine
+      //'SELECT * FROM books;',
+        'SELECT time, amount FROM Transaction WHERE Transaction.cardID = ?',
+      variables: [Variable.withInt(cardID)],
+    ).watch().map((rows){
+      return rows
+          //TODO need to fix this part (need to figure out how to pull out amount and time columns from the query to enter into the list's rows
+          .map((row) => CardTransaction(time.fromData(row.data, this), amount.fromData(row.data, this)))
+          .toList();
+    });
+  }
+
+/*  Stream<List<Transaction>> watchCardTransactions(int cardID) {
+    return (select(transactions)
+      ..where((row) => row.cardID.equals(cardID))
+      ..orderBy([
+            (row) => OrderingTerm(expression: row.time, mode: OrderingMode.asc)
+      ]))
+        .watch();
+  }*/
+
+  String cardBalance(int cardID){
+    return customSelect(
+      //query works fine
+      //'SELECT * FROM books;',
+      'SELECT SUM(amount) FROM [Transactions] WHERE card = ?;',
+      variables: [Variable.withInt(cardID)],
+    ).toString();
+  }
+
+  String highest_transactionID(int cardID){
+    return customSelect(
+      //query works fine
+      //'SELECT * FROM books;',
+      'SELECT MAX(transactionID) FROM Transactions WHERE cardID = ?;',
+      variables: [Variable.withInt(cardID)],
+    ).toString();
+  }
+
+  //Presumably super user stuff
+  Future<List<Transaction>> getAllTransactions() => select(transactions).get();
+
+  Stream<List<Transaction>> watchAllTransactions() =>
+      select(transactions).watch();
+
+  /// for transferring and receiving funds, can use the below (specify type parameter depending on which one
+  /// is being done) and play around with validation and finish parameter?
+  Future insertTransactions(Insertable<Transaction> transaction) =>
+      into(transactions).insert(transaction);
+  Future updateTransactions(Insertable<Transaction> transaction) =>
+      update(transactions).replace(transaction);
+  Future deleteTransactions(Insertable<Transaction> transaction) =>
+      delete(transactions).delete(transaction);
 }
 
-@UseDao(tables: [Accounts])
+/*@UseDao(tables: [Accounts])
 class AccountDao extends DatabaseAccessor<AppDatabase> with _$AccountDaoMixin {
   final AppDatabase db;
 
@@ -144,8 +306,7 @@ class AcDao extends DatabaseAccessor<AppDatabase> with _$AcDaoMixin {
 }
 
 @UseDao(tables: [Transactions])
-class TransactionDao extends DatabaseAccessor<AppDatabase>
-    with _$TransactionDaoMixin {
+class TransactionDao extends DatabaseAccessor<AppDatabase> with _$TransactionDaoMixin {
   final AppDatabase db;
 
   // Called by the AppDatabase class
@@ -175,4 +336,4 @@ class TransactionDao extends DatabaseAccessor<AppDatabase>
       update(transactions).replace(transaction);
   Future deleteTransactions(Insertable<Transaction> transaction) =>
       delete(transactions).delete(transaction);
-}
+}*/
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'home_page.dart';
+import 'DB/drift_database.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,103 +11,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Login(),
-    );
-  }
-}
-
-class Login extends StatefulWidget {
-  @override
-  _LoginState createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-          title: const Text(
-            'Login',
-            style: TextStyle(color: Colors.black),
-          ),
-          backgroundColor: Colors.white),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 40.0),
-              child: Center(
-                child: Container(
-                  width: 300,
-                  height: 225,
-                  child: Image.asset('assets/images/simucash_logo_light.png'),
-                ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Username',
-                    hintText: 'Enter a valid user name'),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(
-                  left: 15.0, right: 15.0, top: 15.0, bottom: 0.0),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Enter secure password',
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                // Forgot password
-                // TODO FORGOT PASSWORD SCREEN GOES HERE
-              },
-              child: const Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
-              ),
-            ),
-            Container(
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: const Color(0xffbe9e44),
-                  borderRadius: BorderRadius.circular(10)),
-              child: TextButton(
-                onPressed: () {
-                  // This makes sense :o
-                  // Login Button
-                  // USE THIS IN PRODUCTION
-                  // Navigator.of(context).pushReplacement(
-                  //     MaterialPageRoute(builder: (_) => HomePage()));
-                  // USE THIS FOR TESTING
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => HomePage()));
-                },
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 80,
-            ),
-            const Text('New User? Create Account'),
-          ],
-        ),
+    return Provider(
+      create: (context) => AppDatabase(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Login(),
       ),
+
     );
   }
 }
+
+
